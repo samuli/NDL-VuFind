635831a89909032007bcd463728ce576d21f2e6f
Kalle Pyykkönen <kalle.pyykkonen@helsinki.fi>
2013-10-14 16:32:27 +0300

    [ALLI-1991] Removed unused function.

--- web/interface/themes/ndl/js/init_fancybox.js
+++ web/interface/themes/ndl/js/init_fancybox.js
@@ -25,38 +25,3 @@ $(document).ready(function() {
                               }
                     });
 });
-
-function launchFancybox(el) {
-    var hrefs = new Array();
-    hrefs.push($(el).attr('href').replace('&index=0', ''));
-    var group = $(el).attr('rel');
-    var dates = $(el).data('dates');
-    var photoTitle = $(el).data('title');
-    var building = $(el).data('building');
-    var author = $(el).data('author');
-    var url = $(el).data('url');
-    var linkText = $(el).data('linktext')
-    $('a[rel="'+group+'"]').each(function(){
-        var href = $(this).attr('href').replace('&index=0', '');
-        if ($.inArray(href, hrefs) === -1) {
-            hrefs.push(href);
-        }
-    });
-    $.fancybox.open(hrefs, 
-                    {   type: 'image', 
-                        nextEffect: 'fade', 
-                        prevEffect: 'fade',
-                        afterLoad   : function() {
-                            this.title = '<h3 class="fancyTitle">'+photoTitle+'</h3>'
-                                             +'<div class="fancyAuthor">'+author+'</div>'
-                                             +'<div class="fancyBuilding">'+building+'</div>'
-                                             +'<div class="fancyDates">'+dates+'</div>'
-                                             +'<div class="fancyLink"><a href="'+url+'">'+linkText+'</a></div>';
-                                         
-                        },
-                        helpers : {
-                            title: {type: 'inside'}                            
-                        }
-                    }
-    );
-}
