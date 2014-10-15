98a98db69783344f223e59afb8d89fd4fb56829e
Samuli Sillanpää <samuli.sillanpaa@helsinki.fi>
2014-10-15 09:16:55 +0300

    Improvements

--- web/services/MyResearch/Fines.php
+++ web/services/MyResearch/Fines.php
@@ -186,7 +186,7 @@ class Fines extends MyResearch
                         $row['type'] = $row['fine'];
                         if (!$row['title'] || $row['title'] === '') {
                             $row['title'] = translate('not_applicable');
-			    $row['id'] = null;
+                            $row['id'] = null;
                         }
                         
                     }
