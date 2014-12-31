427060a8362a2f92f3e0045ee4e13df5deb5121f
Ere Maijala <ere.maijala@helsinki.fi>
2014-12-31 14:20:39 +0200

    AWS: Added a note that changing password is not yet available.

--- web/Drivers/AxiellWebServices.php
+++ web/Drivers/AxiellWebServices.php
@@ -1257,6 +1257,7 @@ class AxiellWebServices implements DriverInterface
      * @param String $cardDetails Patron card data
      *
      * @return array Response
+     * @todo This function is not available at the moment
      */
     public function changePassword($cardDetails)
     {
